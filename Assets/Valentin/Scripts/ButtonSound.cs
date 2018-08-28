fd37536183f1093ce01900284eec8310a4334d42
Valentin Maubian <v.maubian@student.isartdigital.com>
2018-08-28 09:14:39 +0200

    Sound Menu

---
+++ Assets/Valentin/Scripts/ButtonSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, ISelectHandler
+{
+    public AudioSource mySource;
+
+    //Do this when the selectable UI object is selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        mySource.Play();
+    }
+}
